6f1c5bc5be7ef76bb556b963b456c557e5fd0436
Ross Wintle <ross@oikos.org.uk>
2015-07-05 21:56:11 +0100

    Set text domain and improved translatability (need to improve this in scripts!)

--- admin/class-wp-quick-image-admin.php
+++ admin/class-wp-quick-image-admin.php
@@ -107,8 +107,12 @@ class WP_Quick_Image_Admin {
 	 */
 	public function init_settings_menu() {
 		
-		add_options_page( 'WP Quick Image', 'WP Quick Image', 'manage_options', 'wp_quick_image', array($this, 'settings_page') );
-
+		add_options_page(
+				__('WP Quick Image', 'wp-quick-image'),
+		 		__('WP Quick Image', 'wp-quick-image'),
+		 		'manage_options',
+		 		'wp_quick_image',
+		 		array($this, 'settings_page') );
 	}
 
 	/**
@@ -123,14 +127,14 @@ class WP_Quick_Image_Admin {
 
 		add_settings_section(
 			'wpqi_settings_general', 
-			__( 'General settings', 'wordpress' ), 
+			__( 'General settings', 'wp-quick-image' ), 
 			array($this, 'wpqi_settings_general_callback'), 
 			'wp_quick_image'
 		);	
 
 		add_settings_field( 
 			'wpqi_post_type', 
-			__( 'Post type', 'wordpress' ), 
+			__( 'Post type', 'wp-quick-image' ), 
 			array($this, 'wpqi_post_type_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -138,7 +142,7 @@ class WP_Quick_Image_Admin {
 	
 		add_settings_field( 
 			'wpqi_category', 
-			__( 'Category', 'wordpress' ), 
+			__( 'Category', 'wp-quick-image' ), 
 			array($this, 'wpqi_category_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -146,7 +150,7 @@ class WP_Quick_Image_Admin {
 
 		add_settings_field( 
 			'wpqi_tag', 
-			__( 'Tag', 'wordpress' ), 
+			__( 'Tag', 'wp-quick-image' ), 
 			array($this, 'wpqi_tag_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -154,7 +158,7 @@ class WP_Quick_Image_Admin {
 
 		add_settings_field( 
 			'wpqi_add_excerpt', 
-			__( 'Add excerpt', 'wordpress' ), 
+			__( 'Add excerpt', 'wp-quick-image' ), 
 			array($this, 'wpqi_add_excerpt_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -162,7 +166,7 @@ class WP_Quick_Image_Admin {
 
 		add_settings_field( 
 			'wpqi_add_content', 
-			__( 'Add content', 'wordpress' ), 
+			__( 'Add content', 'wp-quick-image' ), 
 			array($this, 'wpqi_add_content_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -170,7 +174,7 @@ class WP_Quick_Image_Admin {
 
 		add_settings_field( 
 			'wpqi_set_featured_image', 
-			__( 'Set featured image', 'wordpress' ), 
+			__( 'Set featured image', 'wp-quick-image' ), 
 			array($this, 'wpqi_set_featured_image_field_render'), 
 			'wp_quick_image', 
 			'wpqi_settings_general' 
@@ -206,7 +210,7 @@ class WP_Quick_Image_Admin {
 	 * @since 0.2.0
 	 */
 	public function wpqi_settings_general_callback() {
-		_e('General settings for WP Quick Image', 'wordpress');
+		_e('General settings for WP Quick Image', 'wp-quick-image');
 	}
 
 	/**
@@ -228,7 +232,7 @@ class WP_Quick_Image_Admin {
 				<option value='<?php echo $this_type ?>' <?php selected( $current_val, $this_type ); ?>><?php echo $this_type; ?></option>
 			<?php endforeach; ?>
 		</select>
-		<p><em><?php _e('Select a post type to create when creating a quick image. This will normally be "post", but change it if you need to.'); ?></em></p>
+		<p><em><?php _e('Select a post type to create when creating a quick image. This will normally be "post", but change it if you need to.', 'wp-quick-image'); ?></em></p>
 
 	<?php
 	}
@@ -247,12 +251,12 @@ class WP_Quick_Image_Admin {
 		}
 	?>
 		<select name='wpqi_settings[category]'>
-			<option value="0" <?php selected( $current_val, 0 ); ?>><?php _e("No category"); ?></option>
+			<option value="0" <?php selected( $current_val, 0 ); ?>><?php _e("No category", 'wp-quick-image'); ?></option>
 			<?php foreach (get_categories(array('hide_empty' => 0)) as $this_cat) : ?>
 				<option value='<?php echo $this_cat->term_id ?>' <?php selected( $current_val, $this_cat->term_id ); ?>><?php echo $this_cat->name; ?></option>
 			<?php endforeach; ?>
 		</select>
-		<p><em><?php _e('Please select a category to assign new quick images to. This only applies if the post type is "Post". Note that the WordPress default category applies if this is not set.'); ?></em></p>
+		<p><em><?php _e('Please select a category to assign new quick images to. This only applies if the post type is "Post". Note that the WordPress default category applies if this is not set.', 'wp-quick-image'); ?></em></p>
 
 	<?php
 	}
@@ -272,12 +276,12 @@ class WP_Quick_Image_Admin {
 
 	?>
 		<select name='wpqi_settings[tag]'>
-			<option value="0" <?php selected( $current_val, 0 ); ?>><?php _e('No tag'); ?></option>
+			<option value="0" <?php selected( $current_val, 0 ); ?>><?php _e('No tag', 'wp-quick-image'); ?></option>
 			<?php foreach (get_tags(array('hide_empty' => false)) as $this_tag) : ?>
 				<option value='<?php echo $this_tag->term_id; ?>' <?php selected( $current_val, $this_tag->term_id ); ?>><?php echo $this_tag->name; ?></option>
 			<?php endforeach; ?>
 		</select>
-		<p><em><?php _e('Select a tag to assign new posts to. This only applies if the post type is "Post".'); ?></em></p>
+		<p><em><?php _e('Select a tag to assign new posts to. This only applies if the post type is "Post".', 'wp-quick-image'); ?></em></p>
 
 	<?php
 	}
@@ -296,9 +300,9 @@ class WP_Quick_Image_Admin {
 		}
 		
 	?>
-		<input name='wpqi_settings[add_excerpt]' type="radio" value="0" <?php checked($current_val, 0); ?>>No</input>
-		<input name='wpqi_settings[add_excerpt]' type="radio" value="1" <?php checked($current_val, 1); ?>>Yes</input>
-		<p><em>If this is turned on, the content added in quick image will also be added to the excerpt field. This can be useful for populating OpenGraph data for social media to use.</em></p>
+		<input name='wpqi_settings[add_excerpt]' type="radio" value="0" <?php checked($current_val, 0); ?>><?php _e('No', 'wp-quick-image'); ?></input>
+		<input name='wpqi_settings[add_excerpt]' type="radio" value="1" <?php checked($current_val, 1); ?>><?php _e('Yes', 'wp-quick-image'); ?></input>
+		<p><em><?php _e('If this is turned on, the content added in quick image will also be added to the excerpt field. This can be useful for populating OpenGraph data for social media to use.', 'wp-quick-image'); ?></em></p>
 	<?php
 	}
 
@@ -316,9 +320,9 @@ class WP_Quick_Image_Admin {
 		}
 		
 	?>
-		<input name='wpqi_settings[add_content]' type="radio" value="0" <?php checked($current_val, 0); ?>>No</input>
-		<input name='wpqi_settings[add_content]' type="radio" value="1" <?php checked($current_val, 1); ?>>Yes</input>
-		<p><em>If this is turned on, the image will be added to the content of the post.</em></p>
+		<input name='wpqi_settings[add_content]' type="radio" value="0" <?php checked($current_val, 0); ?>><?php _e('No', 'wp-quick-image'); ?></input>
+		<input name='wpqi_settings[add_content]' type="radio" value="1" <?php checked($current_val, 1); ?>><?php _e('Yes', 'wp-quick-image'); ?></input>
+		<p><em><?php _e('If this is turned on, the image will be added to the content of the post.', 'wp-quick-image'); ?></em></p>
 	<?php
 	}
 
@@ -336,9 +340,9 @@ class WP_Quick_Image_Admin {
 		}
 		
 	?>
-		<input name='wpqi_settings[set_featured_image]' type="radio" value="0" <?php checked($current_val, 0); ?>>No</input>
-		<input name='wpqi_settings[set_featured_image]' type="radio" value="1" <?php checked($current_val, 1); ?>>Yes</input>
-		<p><em>If this is turned on, the image will be used as the featured image of the post.</em></p>
+		<input name='wpqi_settings[set_featured_image]' type="radio" value="0" <?php checked($current_val, 0); ?>><?php _e('No', 'wp-quick-image'); ?></input>
+		<input name='wpqi_settings[set_featured_image]' type="radio" value="1" <?php checked($current_val, 1); ?>><?php _e('Yes', 'wp-quick-image'); ?></input>
+		<p><em><?php _e('If this is turned on, the image will be used as the featured image of the post.', 'wp-quick-image'); ?></em></p>
 	<?php
 	}
 
@@ -348,7 +352,7 @@ class WP_Quick_Image_Admin {
 	 * @since 0.1.0
 	 */
 	public function init_widget() {
-		wp_add_dashboard_widget( 'wp-quick-image', 'Quick Image Post', array($this, 'display_widget') );
+		wp_add_dashboard_widget( 'wp-quick-image', __('Quick Image Post', 'wp-quick-image'), array($this, 'display_widget') );
 	}
 
 	/**
@@ -363,19 +367,19 @@ class WP_Quick_Image_Admin {
 		
 			<div class="input-text-wrap" id="wp-quick-image-title-wrap">
 				<label class="prompt" for="wp-quick-image-title" id="wp-quick-image-title-prompt-text">
-					Title
+					<?php _e('Title', 'wp-quick-image'); ?>
 				</label>
 				<input type="text" name="wp-quick-image-title" id="wp-quick-image-title" autocomplete="off">
 			</div>
 
-			<button class="button button-secondary wpqi-disable-on-submit" id="wp-quick-image-choose">Add image</button>
+			<button class="button button-secondary wpqi-disable-on-submit" id="wp-quick-image-choose"><?php _e('Add image', 'wp-quick-image'); ?></button>
 
 			<div id="wp-quick-image-preview">
 				<input name="wp-quick-image-id" id="wp-quick-image-id" type="hidden" value="0">
 			</div>
 
 			<div class="textarea-wrap" id="wp-quick-image-description-wrap">
-				<label class="prompt" for="wp-quick-image-content" id="wp-quick-image-content-prompt-text">Post content</label>
+				<label class="prompt" for="wp-quick-image-content" id="wp-quick-image-content-prompt-text"><?php _e('Post content', 'wp-quick-image'); ?></label>
 				<textarea name="wp-quick-image-content" id="wp-quick-image-content" class="mceEditor" rows="3" cols="15" autocomplete="off"></textarea>
 			</div>
 
@@ -383,7 +387,7 @@ class WP_Quick_Image_Admin {
 				<input type="hidden" name="action" id="wp-quick-image-action" value="wp-quick-image-save">
 				<input type="hidden" name="post_type" value="post">
 				<?php wp_nonce_field('wp-quick-image'); ?>
-				<input type="submit" name="save" id="wpqi-save-post" class="button button-primary wpqi-disable-on-submit" value="Publish this">
+				<input type="submit" name="save" id="wpqi-save-post" class="button button-primary wpqi-disable-on-submit" value="<?php _e('Publish this', 'wp-quick-image'); ?>">
 				<br class="clear">
 			</p>
 
